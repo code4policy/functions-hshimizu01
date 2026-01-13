9eee1ca3e25b0951b22db942212ff833da248d90
bherceha <bhercehagiwara@hsph.harvard.edu>
2026-01-13 18:35:04 -0500

    typo

--- calculator.py
+++ calculator.py
@@ -11,6 +11,6 @@ def divide(a,b):
 	return a/b
 
 
-print("I'm going use the calculator functions to multiply 5 and 6")
+print("I'm going to use the calculator functions to multiply 5 and 6")
 x = multiply(5,6)
 print(x)
