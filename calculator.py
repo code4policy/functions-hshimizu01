14f491fa39cad45865e60b8cc976d0491094b0c6
hshimizu01 <hshimizu@hks.harvard.edu>
2026-01-18 09:42:35 -0500

    added square and cube finallyy

--- calculator.py
+++ calculator.py
@@ -10,6 +10,32 @@ def multiply(a, b):
 def divide(a, b):
 	return a / b
 
+def square(a):
+	return a * a
+
+def cube(a):
+	return a * a * a
+
+def square_n_times(number, n):
+	updated_number = 0
+	changed_number = number
+	for times in range(n):
+		changed_number = changed_number * changed_number
+		updated_number = updated_number + changed_number
+	return updated_number
+
 print("I'm going use the calculator functions to divide 30 by 6")
 x = divide(30,6)
 print(x) 
+
+# print("I'm going use the calculator functions to raise 30 by the power of 2")
+# x = square(30)
+# print(x) 
+
+# print("I'm going use the calculator functions to determine 30 cubed")
+# x = cube(30)
+# print(x) 
+
+# print("I'm going use the calculator functions to determine 3 cubed")
+# x = square_n_times(3, 3)
+# print(x) 
